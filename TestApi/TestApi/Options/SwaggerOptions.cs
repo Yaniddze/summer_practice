6c2d7542781ccdb6528c55b929b123c804129612
Yan <yanrus09@gmail.com>
2020-05-31 20:20:49 +0500

    Add Swagger

---
+++ TestApi/TestApi/Options/SwaggerOptions.cs
@@ -0,0 +1,9 @@
+namespace TestApi.Options
+{
+    public class SwaggerOptions
+    {
+        public string JsonRoute { get; set; }
+        public string Description { get; set; }
+        public string UiEndpoint { get; set; }
+    }
+}
--- TestApi/TestApi/Startup.cs
+++ TestApi/TestApi/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using TestApi.Options;
 
 namespace TestApi
 {
@@ -38,6 +40,14 @@ namespace TestApi
                     builder.AllowAnyMethod();
                 });
             });
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "MyApi",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,8 +58,15 @@ namespace TestApi
                 app.UseDeveloperExceptionPage();
             }
 
-//            app.UseHttpsRedirection();
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
 
+            app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; });
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
+            
             app.UseRouting();
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions()
